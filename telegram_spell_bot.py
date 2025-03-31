f22796180a73170f9db0076974eff065b611e284
Dung241 <minhdung.tran.241@gmail.com>
2025-03-31 20:49:04 +0700

    telegram_spell_bot.py

--- telegram_spell_bot.py
+++ telegram_spell_bot.py
@@ -1,3 +1,4 @@
+
 # ===== 1. Import thư viện =====
 import telebot
 import re
@@ -189,7 +190,7 @@ print('Bot is running...')
 
 # ===== 3. Chạy Flask để giữ Web Service hoạt động =====
 TOKEN = "7922091397:AAHpyLRpiXr_IkDMFLPjy-IR048-RE_SZKI"
-WEBHOOK_URL="https://telebot-1-io0s.onrender.com/7922091397:AAHpyLRpiXr_IkDMFLPjy-IR048-RE_SZKI"
+WEBHOOK_URL = f"https://telebot-1-io0s.onrender.com/{TOKEN}"
 
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
@@ -198,7 +199,7 @@ app = Flask(__name__)
 def home():
     return "Bot is running!"
 
-@app.route('/7922091397:AAHpyLRpiXr_IkDMFLPjy-IR048-RE_SZKI', methods=['POST'])
+@app.route(f'/{TOKEN}', methods=['POST'])
 def receive_update():
     update = request.get_json()
     if update:
@@ -211,8 +212,7 @@ if __name__ == "__main__":
 
     # Thiết lập webhook
     bot.remove_webhook()
-    bot.set_webhook(url="https://telebot-1-io0s.onrender.com/7922091397:AAHpyLRpiXr_IkDMFLPjy-IR048-RE_SZKI")
+    bot.set_webhook(url=WEBHOOK_URL)
 
     # Chạy Flask server
     app.run(host="0.0.0.0", port=port)
-
